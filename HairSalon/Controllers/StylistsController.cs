2f7162a4c288bac423d9fe9b1f33ed235102be06
jakechaley <jakechaley@gmail.com>
2022-03-11 10:30:25 -0800

    Add home controller code. Start working on Stylists Controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+  }
+}
