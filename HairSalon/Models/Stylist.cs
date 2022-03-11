01cadcb56ced9b63b2a2f4f7b43be4530c09cf01
jakechaley <jakechaley@gmail.com>
2022-03-11 10:05:20 -0800

    write out different Models

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace BestRestaurant.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Restaurant> Restaurants { get; set; }
+  }
+}
